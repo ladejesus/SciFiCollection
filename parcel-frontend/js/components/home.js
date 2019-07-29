8b1034eaeb1e7220f637683dab5ed1bf7b9e9929
ladejesus <luisito.dejesus@gmail.com>
2019-07-29 08:40:19 -0400

    changed the home page welcome and color in button hover.

--- parcel-frontend/js/components/home.js
+++ parcel-frontend/js/components/home.js
@@ -1,5 +1,5 @@
 export default function Home(){
     return `
     <h1>Welcome to the Final Frontier</h1>
-        <p>Make your selection using the options below.</p>`
+        <p>Begin your journey by using the Nav above.</p>`
 }
